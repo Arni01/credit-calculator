2988fa99b4a909ed3f6c4a8a7da95ca245e68193
Artsem Mukha <artsemmukha@gmail.com>
2022-07-15 12:44:14 +0300

    feat: added editorconfig, prittier, eslint

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,10 @@
-export { default as CreditCalculator } from './CreditCalculator/CreditCalculator';
-export { default as AllCreditDetails } from './CreditCalculator/AllCreditDetails/AllCreditDetails';
-export { default as CalculationCreditInfo } from './CreditCalculator/CalculationCreditInfo/CalculationCreditInfo';
-export { default as DataEntry } from './CreditCalculator/DataEntry/DataEntry';
+export { default as CreditCalculator } from './CreditCalculator/CreditCalculator'
+export { default as AllCreditDetails } from './CreditCalculator/AllCreditDetails/AllCreditDetails'
+export { default as CalculationCreditInfo } from './CreditCalculator/CalculationCreditInfo/CalculationCreditInfo'
+export { default as DataEntry } from './CreditCalculator/DataEntry/DataEntry'
 
 // common
-export { default as AmountList } from './common/AmountList/AmountList';
-export { default as CustomInput } from './common/Input/CustomInput';
-export { default as CreditDescription } from './common/List/CreditDescription';
-export { default as CustomSelector } from './common/Selector/CustomSelector';
+export { default as AmountList } from './common/AmountList/AmountList'
+export { default as CustomInput } from './common/Input/CustomInput'
+export { default as CreditDescription } from './common/List/CreditDescription'
+export { default as CustomSelector } from './common/Selector/CustomSelector'
