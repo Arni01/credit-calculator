aa5de4b96a81709a32db89eb02395180a255e128
Artsem Mukha <artsemmukha@gmail.com>
2022-07-15 16:59:59 +0300

    fixed(date): fixed view date

--- src/helpers/creditMetods/creditMetods.ts
+++ src/helpers/creditMetods/creditMetods.ts
@@ -1,152 +1,160 @@
-import { IStateReducer } from 'state/stateReducer';
-import { DefaultCreditRate, TCreditPeriodItem } from '../constants/CreditData';
+import { IStateReducer } from 'state/stateReducer'
+import { DefaultCreditRate, TCreditPeriodItem } from '../constants/CreditData'
 
 // функция работает при первом включении
 export const getFullRate = (refinancingRate: number) =>
-  refinancingRate + DefaultCreditRate;
+    refinancingRate + DefaultCreditRate
 
 // подсчет месячного процента
 const getPercentage = (rate: number) => {
-  return rate > 0 ? rate / 100 / 12 : 0;
-};
+    return rate > 0 ? rate / 100 / 12 : 0
+}
 
 // подсчет месчного платежа (ссылку на формулу см в readme.md)
 const getMonthlyPayment = (sum: number, period: number, percentage: number) => {
-  if (percentage === 0) return 0;
+    if (percentage === 0) return 0
 
-  const result =
-    sum * (percentage + percentage / ((1 + percentage) ** period - 1));
+    const result =
+        sum * (percentage + percentage / ((1 + percentage) ** period - 1))
 
-  return result;
-};
+    return result
+}
 
 // подсчет общего платежа
 const getTotalAmount = (monthlyPayment: number, period: number) => {
-  return monthlyPayment > 0 ? monthlyPayment * period : 0;
-};
+    return monthlyPayment > 0 ? monthlyPayment * period : 0
+}
 
 // подсчет переплаты
 const getTotalOverPayment = (sum: number, totalAmount: number) => {
-  return totalAmount > 0 ? totalAmount - sum : 0;
-};
+    return totalAmount > 0 ? totalAmount - sum : 0
+}
 
 export interface IGetCalculationCreditInfo {
-  monthlyPayment: number;
-  totalAmount: number;
-  totalOverPayment: number;
-  percentage: number;
+    monthlyPayment: number
+    totalAmount: number
+    totalOverPayment: number
+    percentage: number
 }
 
 // функциия для получения данных первичных данных о кредите(без детальных)
 export const getCalcutionCreditInfo = ({
-  creditRate,
-  currentPeriod,
-  creditSum,
-}: IStateReducer): IGetCalculationCreditInfo => {
-  const percentage = getPercentage(creditRate);
-  const monthlyPayment = getMonthlyPayment(
+    creditRate,
+    currentPeriod,
     creditSum,
-    currentPeriod.value,
-    percentage
-  );
-  const totalAmount = getTotalAmount(monthlyPayment, currentPeriod.value);
-  const totalOverPayment = getTotalOverPayment(creditSum, totalAmount);
+}: IStateReducer): IGetCalculationCreditInfo => {
+    const percentage = getPercentage(creditRate)
+    const monthlyPayment = getMonthlyPayment(
+        creditSum,
+        currentPeriod.value,
+        percentage
+    )
+    const totalAmount = getTotalAmount(monthlyPayment, currentPeriod.value)
+    const totalOverPayment = getTotalOverPayment(creditSum, totalAmount)
 
-  return { monthlyPayment, totalAmount, totalOverPayment, percentage };
-};
+    return { monthlyPayment, totalAmount, totalOverPayment, percentage }
+}
 
-const getRoundNumber = (number: number) => Number(number.toFixed(2));
+const getRoundNumber = (number: number) => Number(number.toFixed(2))
 
 // подстчет последнего платяже с учетом разницы между общими выплатами и помесячными * период выплат
 export const getLastMonthlyPayment = (
-  monthlyPayment: number,
-  totalAmount: number,
-  currentPeriod: TCreditPeriodItem
+    monthlyPayment: number,
+    totalAmount: number,
+    currentPeriod: TCreditPeriodItem
 ) => {
-  const roundMonthlyPayment = getRoundNumber(monthlyPayment);
-  const roundTotalAmount = getRoundNumber(totalAmount);
-  return getRoundNumber(
-    monthlyPayment +
-      (roundTotalAmount - roundMonthlyPayment * currentPeriod.value)
-  );
-};
+    const roundMonthlyPayment = getRoundNumber(monthlyPayment)
+    const roundTotalAmount = getRoundNumber(totalAmount)
+    return getRoundNumber(
+        monthlyPayment +
+            (roundTotalAmount - roundMonthlyPayment * currentPeriod.value)
+    )
+}
 
 type TDate = {
-  year: number;
-  month: number;
-  day: number;
-};
+    year: number
+    month: number
+    day: number
+}
 
 const getDate = (date: TDate) => {
-  const result = { ...date };
-  if (result.month + 1 > 12) {
-    result.year += 1;
-    result.month = 1;
-  } else {
-    result.month += 1;
-  }
+    const result = { ...date }
+    if (result.month + 1 > 12) {
+        result.year += 1
+        result.month = 1
+    } else {
+        result.month += 1
+    }
 
-  return result;
-};
+    return result
+}
 
 type TGetAllCreditDetails = {
-  sum: number;
-  monthlyPayment: number;
-  lastMonthlyPayment: number;
-  currentPeriod: number;
-  percentage: number;
-};
+    sum: number
+    monthlyPayment: number
+    lastMonthlyPayment: number
+    currentPeriod: number
+    percentage: number
+}
+
+const getStringDate = (date: TDate) => {
+    const month = date.month.toString()
+
+    return `${date.day}.${month.length === 1 ? '0' + month : month}.${
+        date.year
+    }`
+}
 
 // функция подсчета дополнительной информации об кредите
 export const getAllCreditDatails = ({
-  sum,
-  monthlyPayment,
-  lastMonthlyPayment,
-  currentPeriod,
-  percentage,
+    sum,
+    monthlyPayment,
+    lastMonthlyPayment,
+    currentPeriod,
+    percentage,
 }: TGetAllCreditDetails) => {
-  const currentDate = new Date();
-  let dateObject: TDate = {
-    year: currentDate.getFullYear(),
-    month: currentDate.getMonth() + 1,
-    day: currentDate.getDate(),
-  };
-  let balance = sum;
+    const currentDate = new Date()
+    let dateObject: TDate = {
+        year: currentDate.getFullYear(),
+        month: currentDate.getMonth() + 1,
+        day: currentDate.getDate(),
+    }
+    let balance = sum
 
-  const result = [];
+    const result = []
 
-  for (let i = 1; i <= currentPeriod; i++) {
-    const isLastMonth = i === currentPeriod;
+    for (let i = 1; i <= currentPeriod; i++) {
+        const isLastMonth = i === currentPeriod
 
-    // дата платежа
-    dateObject = getDate(dateObject);
+        // дата платежа
+        dateObject = getDate(dateObject)
 
-    // платёж по процентам
-    const interestPayment = getRoundNumber(balance * percentage);
+        // платёж по процентам
+        const interestPayment = getRoundNumber(balance * percentage)
 
-    // общая сумма платежа
-    const currentMonthlyPayment = getRoundNumber(
-      isLastMonth ? lastMonthlyPayment : monthlyPayment
-    );
+        // общая сумма платежа
+        const currentMonthlyPayment = getRoundNumber(
+            isLastMonth ? lastMonthlyPayment : monthlyPayment
+        )
 
-    // платёж по основному долгу
-    const mainDebit = getRoundNumber(
-      isLastMonth ? balance : monthlyPayment - interestPayment
-    );
+        // платёж по основному долгу
+        const mainDebit = getRoundNumber(
+            isLastMonth ? balance : monthlyPayment - interestPayment
+        )
 
-    // остаток по телу кредита после данного платежа
-    const remainingDebt = getRoundNumber(balance - mainDebit);
-    balance = remainingDebt;
+        // остаток по телу кредита после данного платежа
+        const remainingDebt = getRoundNumber(balance - mainDebit)
+        balance = remainingDebt
 
-    result.push({
-      id: i,
-      datePayment: `${dateObject.day}.${dateObject.month}.${dateObject.year}`,
-      interestPayment,
-      mainDebit,
-      monthlyPayment: currentMonthlyPayment,
-      remainingDebt,
-    });
-  }
+        result.push({
+            id: i,
+            datePayment: getStringDate(dateObject),
+            interestPayment,
+            mainDebit,
+            monthlyPayment: currentMonthlyPayment,
+            remainingDebt,
+        })
+    }
 
-  return result;
-};
+    return result
+}
